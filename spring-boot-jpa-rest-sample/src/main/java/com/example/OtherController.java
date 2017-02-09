6092310e6772d142297b638760e2ccb2e5f0dd62
ParkSeongMin <codingstory79@gmail.com>
2017-02-09 18:10:12 +0900

    modify restcontroller

--- spring-boot-jpa-rest-sample/src/main/java/com/example/OtherController.java
+++ spring-boot-jpa-rest-sample/src/main/java/com/example/OtherController.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest")
 public class OtherController {
 
 	@Autowired
 	private PersonRepository personRepository;
 	
-	@RequestMapping("/getAll")
+	@RequestMapping(method=RequestMethod.GET)
 	public List<Person> getAll() {
 		return (List<Person>) personRepository.findAll();
 	}
